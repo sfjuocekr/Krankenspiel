437a573bcf7df6ba7c0d77e5687868357905fd0d
Sjoer van der Ploeg <sfjuocekr@gmail.com>
2015-10-05 14:33:00 +0200

    Some things
    I changed project build settings, so preprocessing is optimized for the
    BT-200. This should increase the size of the final build, but will
    increase performance significantly.
    
    Do not pay attention to the scene files yet, those are just for testing.
    I am currently testing how to properly make the AR camera behave
    independent of the object being tracked, and I will eventually look into
    merging the Moverio Controller with the ARCamera so we can use sensors
    etc as intended.
    
    Priority right now is getting frame markers to work without the camera
    changing the world center to the marker.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody _rigidbody;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _rigidbody = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+
+    void FixedUpdate ()
+    {
+        Vector3 _position = transform.position;
+
+        _position.z = 0;
+        transform.position = _position;
+    }
+}
