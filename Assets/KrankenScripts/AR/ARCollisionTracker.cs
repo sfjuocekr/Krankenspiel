fdf47980c583b383d1cff3344a24196cafbf40ef
Sjoer van der Ploeg <sfjuocekr@gmail.com>
2015-10-14 12:21:50 +0200

    Added a way to track time on the AR objects and  trigger something
    Should probably make an event out of this, but it works as it should
    right now.

---
+++ Assets/KrankenScripts/AR/ARCollisionTracker.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ARCollisionTracker : MonoBehaviour
+{
+    public List<GameObject> TrackedObjects;
+    private Dictionary<GameObject, float> _collisions = new Dictionary<GameObject, float>();
+
+    void OnTriggerEnter(Collider _collider)
+    {
+        if (_collider.gameObject == TrackedObjects.Contains(_collider.gameObject))
+        {
+            float _collisionTime;
+
+            if (!_collisions.TryGetValue(_collider.gameObject, out _collisionTime))
+            {
+                _collisions.Add(_collider.gameObject, 0.0f);
+            }
+        }
+    }
+
+    void OnTriggerStay(Collider _collider)
+    {
+        if (_collider.gameObject == TrackedObjects.Contains(_collider.gameObject))
+        {
+            float _collisionTime;
+
+            if (_collisions.TryGetValue(_collider.gameObject, out _collisionTime))
+            {
+                _collisionTime += Time.deltaTime;
+
+                int _seconds = Mathf.RoundToInt(_collisionTime);
+
+                if (_collisionTime >= _seconds && _collisionTime <= _seconds + Time.deltaTime)
+                    _collider.gameObject.SendMessage("CollisionTime", Mathf.RoundToInt(_seconds));
+
+                _collisions.Remove(_collider.gameObject);
+                _collisions.Add(_collider.gameObject, _collisionTime);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider _collider)
+    {
+        if (_collider.gameObject == TrackedObjects.Contains(_collider.gameObject))
+        {
+            float _collisionTime;
+
+            if (_collisions.TryGetValue(_collider.gameObject, out _collisionTime))
+            {
+                _collider.gameObject.SendMessage("CollisionTime", Mathf.RoundToInt(-1));
+
+                _collisions.Remove(_collider.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/KrankenScripts/Interactables/MessageHandler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MessageHandler : MonoBehaviour
+{
+    public GameObject InteractionObject;
+    public GameObject TimerObject;
+
+    public int TriggerTime = 5;
+
+    void CollisionTime(int _time)
+    {
+        if (_time == TriggerTime)
+        {
+            InteractionObject.SetActive(true);
+            TimerObject.SetActive(false);
+            gameObject.SetActive(false);
+        }
+        else if (_time == -1)
+        {
+            TimerObject.SetActive(false);
+        }
+        else if (_time < TriggerTime)
+        {
+            TimerObject.GetComponent<TextMesh>().text = _time.ToString();
+            TimerObject.SetActive(true);
+        }
+    }
+}
